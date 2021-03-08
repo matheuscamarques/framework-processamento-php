0e093bd9128e9ffcca4359f5a164bfbf390b9bd9
Matheus C Marques <matheuscamarques@gmail.com>
2021-03-07 21:06:53 -0300

    Adicionando Imagick

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ff8b0f65a65249aeeef1bed05f4c5d6bcdc724d9',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ff8b0f65a65249aeeef1bed05f4c5d6bcdc724d9',
+    ),
+    'calcinai/php-imagick' => 
+    array (
+      'pretty_version' => 'v0.1.2',
+      'version' => '0.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '001530b19560b9862ffe78c3ae29ad5dc2549e6d',
+    ),
+  ),
+);
